41ae2d86b2ddb8c958d3ba93c60f33ee87225358
uttamshukla259 <117875151+uttamshukla259@users.noreply.github.com>
2023-04-01 20:25:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your code here
+const container = document.querySelector('.container');
+let draggingDiv = null;
+
+// Drag Start Event
+container.addEventListener('dragstart', (e) => {
+  draggingDiv = e.target;
+});
+
+// Drag Over Event
+container.addEventListener('dragover', (e) => {
+  e.preventDefault();
+});
+
+// Drop Event
+container.addEventListener('drop', (e) => {
+  e.preventDefault();
+  const dropDiv = e.target;
+  if (dropDiv !== draggingDiv && dropDiv.nodeName === 'DIV') {
+    const temp = dropDiv.style.backgroundImage;
+    dropDiv.style.backgroundImage = draggingDiv.style.backgroundImage;
+    draggingDiv.style.backgroundImage = temp;
+  }
+});
